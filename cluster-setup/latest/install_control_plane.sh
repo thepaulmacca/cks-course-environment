b5a03c213f2bc8e2e0d211f99a5bb594ca040bcf
Paul McDonald <me@thepaulmacca.com>
2024-11-20 14:36:52 +0000

    Add cilium CLI install

--- cluster-setup/latest/install_control_plane.sh
+++ cluster-setup/latest/install_control_plane.sh
@@ -184,6 +184,14 @@ mkdir -p ~/.kube
 sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
 
 ### cilium
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+
 cilium install --version 1.16.4
 
 cilium status --wait
